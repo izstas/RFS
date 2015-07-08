22055913f00e6331ce7fbc86d31e4000d497c177
Stanislav Izmalkov <izstas@live.ru>
2015-07-08 15:13:08 +0300

    Initial commit
    Added .gitignore, POM and the main class for the server

---
+++ server/src/main/java/me/izstas/rfs/server/RfsServer.java
@@ -0,0 +1,11 @@
+package me.izstas.rfs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfsServer {
+    public static void main(String[] args) {
+        SpringApplication.run(RfsServer.class, args);
+    }
+}
