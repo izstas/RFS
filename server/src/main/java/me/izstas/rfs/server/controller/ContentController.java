1611b79445754c6db6b90b2597c79f2e8ae8d19e
Stanislav Izmalkov <izstas@live.ru>
2015-07-15 18:00:39 +0300

    Added PUT support for /content/**

--- server/src/main/java/me/izstas/rfs/server/controller/ContentController.java
+++ server/src/main/java/me/izstas/rfs/server/controller/ContentController.java
@@ -1,14 +1,18 @@
 package me.izstas.rfs.server.controller;
 
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import me.izstas.rfs.server.config.web.PathWithinPattern;
 
+import me.izstas.rfs.server.config.web.PathWithinPattern;
 import me.izstas.rfs.server.service.ContentService;
 
 @RestController
@@ -22,5 +26,11 @@ public class ContentController {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(contentService.getContentFromUserPath(path));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void putContent(@PathWithinPattern String path, InputStream input) {
+        contentService.putContentToUserPath(path, new InputStreamResource(input));
     }
 }
