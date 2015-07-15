3fcc7a19b4191766e4926f05b7349bde4adca54c
Stanislav Izmalkov <izstas@live.ru>
2015-07-15 17:47:51 +0300

    Always send application/octet-stream as Content-Type for /content/** endpoint

--- server/src/main/java/me/izstas/rfs/server/controller/ContentController.java
+++ server/src/main/java/me/izstas/rfs/server/controller/ContentController.java
@@ -2,6 +2,8 @@ package me.izstas.rfs.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,9 @@ public class ContentController {
     private ContentService contentService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Resource getContent(@PathWithinPattern String path) {
-        return contentService.getContentFromUserPath(path);
+    public ResponseEntity<Resource> getContent(@PathWithinPattern String path) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(contentService.getContentFromUserPath(path));
     }
 }
