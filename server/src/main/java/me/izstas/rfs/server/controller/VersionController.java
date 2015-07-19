630abc94c5b3507cde281ffed80d357336f12814
Stanislav Izmalkov <izstas@live.ru>
2015-07-19 22:37:55 +0300

    Move version endpoint to / (index), include access information (can read/can write)

--- server/src/main/java/me/izstas/rfs/server/controller/VersionController.java
+++ server/src/main/java/me/izstas/rfs/server/controller/VersionController.java
@@ -1,6 +1,10 @@
 package me.izstas.rfs.server.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +17,22 @@ import me.izstas.rfs.server.model.Version;
  * (for example, allow a user to enter a URL) to validate whether the URL actually points to a RFS API.
  */
 @RestController
-@RequestMapping("/version")
+@RequestMapping("/")
 public class VersionController {
     @Autowired
-    private RfsServerVersion version;
+    private RfsServerVersion serverVersion;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Version getVersion() {
-        return new Version(version.getId(), version.getFullVersion());
+    public Version getVersion(Authentication auth) {
+        List<String> access = new ArrayList<>();
+        for (GrantedAuthority authority : auth.getAuthorities()) {
+            access.add(authority.getAuthority());
+        }
+
+        Version version = new Version();
+        version.setId(serverVersion.getId());
+        version.setVersion(serverVersion.getFullVersion());
+        version.setAccess(access);
+        return version;
     }
 }
--- server/src/main/java/me/izstas/rfs/server/model/Version.java
+++ server/src/main/java/me/izstas/rfs/server/model/Version.java
@@ -1,22 +1,36 @@
 package me.izstas.rfs.server.model;
 
+import java.util.List;
+
 /**
  * @see me.izstas.rfs.server.controller.VersionController
  */
 public class Version {
-    private final String id;
-    private final String version;
-
-    public Version(String id, String version) {
-        this.id = id;
-        this.version = version;
-    }
+    private String id;
+    private String version;
+    private List<String> access;
 
     public String getId() {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getVersion() {
         return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<String> getAccess() {
+        return access;
+    }
+
+    public void setAccess(List<String> access) {
+        this.access = access;
     }
 }
