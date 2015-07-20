5a51dd18468d5257e841e3bc2fa2ab1b96102dd4
Stanislav Izmalkov <izstas@live.ru>
2015-07-20 13:37:13 +0300

    Fix the issue when Spring tries to derive response format from path extension

--- server/src/main/java/me/izstas/rfs/server/config/web/RfsWebMvcConfiguration.java
+++ server/src/main/java/me/izstas/rfs/server/config/web/RfsWebMvcConfiguration.java
@@ -3,12 +3,18 @@ package me.izstas.rfs.server.config.web;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class RfsWebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new PathWithinPatternArgumentResolver());
