ab879a25ea6751d273c679d221d2c894cfec51e8
Stanislav Izmalkov <izstas@live.ru>
2015-07-26 21:52:17 +0300

    Make main method open ServerForm

--- client/src/main/java/me/izstas/rfs/client/RfsClient.java
+++ client/src/main/java/me/izstas/rfs/client/RfsClient.java
@@ -1,6 +1,20 @@
 package me.izstas.rfs.client;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import me.izstas.rfs.client.ui.ServerForm;
+
 public class RfsClient {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            // Should not really hurt, but we probably want to handle it properly later
+        }
+
+        JFrame frame = ServerForm.createFrame();
+        frame.setVisible(true);
     }
 }
