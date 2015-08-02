b1d1fddef694d05c9159b807c5be920088bfb000
Stanislav Izmalkov <izstas@live.ru>
2015-08-02 17:30:37 +0300

    And here goes new launching code for SWT

--- client/src/main/java/me/izstas/rfs/client/RfsClient.java
+++ client/src/main/java/me/izstas/rfs/client/RfsClient.java
@@ -1,24 +1,21 @@
 package me.izstas.rfs.client;
 
-import javax.swing.JFrame;
-import javax.swing.UIManager;
+import org.eclipse.swt.widgets.Display;
 
 import me.izstas.rfs.client.rfs.Rfs;
-import me.izstas.rfs.client.ui.ServerForm;
+import me.izstas.rfs.client.ui.MainWindow;
 
 public class RfsClient {
     public static void main(String[] args) {
         new PreInitializationThread().start();
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e) {
-            // Should not really hurt, but we probably want to handle it properly later
-        }
+        Display display = new Display();
 
-        JFrame frame = ServerForm.createFrame();
-        frame.setVisible(true);
+        MainWindow window = new MainWindow();
+        window.setBlockOnOpen(true);
+        window.open();
+
+        display.dispose();
     }
 
 
