2fcecb1612696a006a01d757db15e0e694b4526e
Stanislav Izmalkov <izstas@live.ru>
2015-08-02 17:09:25 +0300

    Update messages and add icons

--- client/src/main/java/me/izstas/rfs/client/ui/Messages.java
+++ client/src/main/java/me/izstas/rfs/client/ui/Messages.java
@@ -2,11 +2,27 @@ package me.izstas.rfs.client.ui;
 
 import org.eclipse.osgi.util.NLS;
 
+import me.izstas.rfs.client.rfs.RfsAccessException;
+import me.izstas.rfs.client.rfs.RfsAuthenticationException;
+import me.izstas.rfs.client.rfs.RfsResponseException;
+
 /**
  * A class containing all localized messages.
  */
 public final class Messages extends NLS {
-    private static final String BUNDLE_NAME = "me.izstas.rfs.client.ui.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "me.izstas.rfs.client.ui.messages"; // $NON-NLS-1$
+
+    public static String MainWindow_title;
+    public static String MainWindow_menu_server;
+    public static String MainWindow_action_connect;
+    public static String MainWindow_action_refresh;
+    public static String MainWindow_action_exit;
+    public static String MainWindow_tree_column_name;
+    public static String MainWindow_tree_column_size;
+    public static String MainWindow_tree_column_dateModified;
+    public static String MainWindow_tree_column_attributes;
+    public static String MainWindow_tree_loading;
+    public static String MainWindow_tree_error;
 
     public static String ServerDialog_title;
     public static String ServerDialog_url;
@@ -15,18 +31,40 @@ public final class Messages extends NLS {
     public static String ServerDialog_authentication_username;
     public static String ServerDialog_authentication_password;
     public static String ServerDialog_check;
-    public static String ServerDialog_browse;
+    public static String ServerDialog_ok;
     public static String ServerDialog_status_checking;
     public static String ServerDialog_status_success_readOnly;
     public static String ServerDialog_status_success_readWrite;
     public static String ServerDialog_status_failure_url;
-    public static String ServerDialog_status_failure_response;
-    public static String ServerDialog_status_failure_authentication;
     public static String ServerDialog_status_failure_noRead;
-    public static String ServerDialog_status_failure_unknown;
+    public static String ServerDialog_status_failure;
+
+    public static String rfs_error_response;
+    public static String rfs_error_authentication;
+    public static String rfs_error_access;
+    public static String rfs_error_unknown;
+
 
     static {
         initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+
+    /**
+     * Returns a presumably user-friendly description of an error represented by the specified exception.
+     */
+    public static String getForException(Throwable e) {
+        if (e instanceof RfsResponseException) {
+            return rfs_error_response;
+        }
+        if (e instanceof RfsAuthenticationException) {
+            return rfs_error_authentication;
+        }
+        if (e instanceof RfsAccessException) {
+            return rfs_error_access;
+        }
+
+        return rfs_error_unknown;
     }
 
 
